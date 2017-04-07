365feccd5d79e8bb3d9789b7ef0444b801c96765
maxpohlman <maxpohlman@gmail.com>
2017-04-07 15:59:49 -0400

    Friday fixes

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # DNA-Sequence-Checker
 ## By Max Pohlman
 
-This python script will take in one *.fastq file as an argument and return the number of sequences of repeating nucleobases. 
+This python script will take in one *.fastq file as an argument and return one of the following based on a second argument
 
-The script will only count maximum lengths, meaning that a sequence of 5 is not also counted as a sequence of 4, 3, and 2.
+*'repeats' returns number of repeating sequences of the same bases
+
+*'pairs' returns the number of each pair of bases
+
+*A number between 1 and 100 will return the number of kmers of that length (
+
 
 A sample fastq is provided. 
 
 This script must be run in a python environment such as Anaconda.
 
-**Example**
+**Examples**
 ```
-python sequence_checker_max.py sample.fastq
+python sequence_checker_max.py sample.fastq repeats
+python sequence_checker_max.py sample.fastq pairs
+python sequence_checker_max.py sample.fastq 5
 ```
--- sequence_checker_max.py
+++ sequence_checker_max.py
@@ -3,10 +3,11 @@
 #Max Pohlman
 
 import sys
+import math
 f = open(sys.argv[1], 'r')
 dna=f.readlines()
 
-def sequence_counter(inputt):
+def sequence_counter(inputt, lenn):
     output = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]] #defines output matrix
     mylets = ['A','C','G','T'] #defines input letters for loop
     for line_num,line in enumerate(inputt): #looks at each line 
@@ -36,5 +37,39 @@ def sequence_counter(inputt):
         if i == 3:
             for j in range(7):
                 print('There were', output[i][j], 'counts of', j+2, 'back-to-back Ts.')
-                
-sequence_counter(dna)
+
+				
+def pair_checker(inputt):
+	output = {'AA':0,'AC':0,'AG':0,'AT':0,'CA':0,'CC':0,'CG':0,'CT':0,'GA':0,'GC':0,'GG':0,'GT':0,'TA':0,'TC':0,'TG':0,'TT':0}			
+	mylets = ['AA','AC','AG','AT','CA','CC','CG','CT','GA','GC','GG','GT','TA','TC','TG','TT']
+	for line_num,line in enumerate(inputt): #looks at each line 
+		if line_num % 4 ==1: #if line number is divisible by 4, we can do analysis
+			i=0 #sets initial location for char string
+			while i < len(line): #i use while instead of enumerate because it's what I'm used to	
+				for let in mylets: #loops through the pairs
+					if line[i:i+2] == let : #checks if there's a match between the two pairs
+						output[let]+=1 #adds to the correct bin
+						i = i + 1 # moves up the window
+				i=i+1 #moves up the window
+	print(output)
+
+def sequence_length(inputt, ll):
+	output = 0
+	for line_num,line in enumerate(inputt): #looks at each line 
+		if line_num % 4 ==1: #if line number is divisible by 4, we can do analysis
+			kmer = len(line) - ll + 1 #counts number of each kmer
+			output = output + kmer #adds to total
+	print('There are', output, 'kmers of length', ll)
+
+sarg = sys.argv[2]
+
+	
+if sarg == 'repeats':
+	sequence_counter(dna)
+if sarg =='pairs':
+	pair_checker(dna)
+if len(sarg) < 5:
+	ll = int(sarg)
+	sequence_length(dna, ll)
+	
+	
